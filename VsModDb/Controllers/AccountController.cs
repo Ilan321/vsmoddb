722dc7e8fc1cd730e9df0256864aaa74f3ace717
Ilan Berlinbluv <ilan.berlinbluv@gmail.com>
2025-02-07 10:52:17 +0200

    add login endpoint

--- VsModDb/Controllers/AccountController.cs
+++ VsModDb/Controllers/AccountController.cs
@@ -17,6 +17,8 @@ namespace VsModDb.Controllers;
 [Authorize]
 [Route("api/v1/account")]
 public class AccountController(
+    ILogger<AccountController> log,
+    UserManager<User> userManager,
     SignInManager<User> signInManager
 ) : ModDbController
 {
@@ -30,6 +32,50 @@ public class AccountController(
             Username = user.UserName!,
             Email = user.Email!
         };
+    }
+
+    [AllowAnonymous]
+    [HttpPost("login")]
+    public async Task Login(LoginRequest request)
+    {
+        log.LogInformation("Received request to login for user {username}", request.Username);
+
+        var user = await userManager.FindByNameAsync(request.Username);
+
+        if (user is null)
+        {
+            log.LogWarning("Could not find user with username {username}", request.Username);
+
+            throw new StatusCodeException(HttpStatusCode.Unauthorized);
+        }
+
+        var result = await signInManager.PasswordSignInAsync(
+            user,
+            request.Password,
+            isPersistent: request.RememberMe,
+            lockoutOnFailure: true
+        );
+
+        if (result.IsLockedOut)
+        {
+            log.LogWarning("User {username} is locked out", request.Username);
+
+            throw new StatusCodeException(HttpStatusCode.Unauthorized);
+        }
+
+        if (result.IsNotAllowed)
+        {
+            log.LogWarning("User {username} is not allowed to login", request.Username);
+
+            throw new StatusCodeException(HttpStatusCode.Unauthorized);
+        }
+
+        if (!result.Succeeded)
+        {
+            log.LogWarning("Failed to sign in user {username}", request.Username);
+
+            throw new StatusCodeException(HttpStatusCode.Unauthorized);
+        }
     }
 
     [HttpGet("logout")]
---
+++ VsModDb/Models/Requests/Account/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace VsModDb.Models.Requests.Account;
+
+public class LoginRequest
+{
+    public required string Username { get; init; }
+    public required string Password { get; init; }
+    public bool RememberMe { get; init; }
+}
